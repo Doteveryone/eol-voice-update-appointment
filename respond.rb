f4862bfaa3e777f63a773ae9e79dc599e8f77381
Dan Williams <dan@buckleywilliams.com>
2016-06-07 15:41:56 +0100

    Adding the script

--- respond.rb
+++ respond.rb
@@ -10,9 +10,23 @@ set :logging, true
 get '/' do
   content_type 'text/xml'
 
+  lines = [
+    'What do you want to do?',
+    'Are you Lucy?',
+    'What is your date of birth?',
+    'You have two appointments booked. You have an appointment with the cardiologist and one with the dietician. Which one would you like to reschedule?',
+    'Your cardiologist appointment with Dr Martin will be rescheduled. Will your daughter be bringing you to the appointment again?',
+    'Looking at your daughter’s schedule, Dr Martin and your daughter are available on Tuesday at 4.30pm or Wednesday at 10am. Which one would you like to book?',
+    'Tuesday at 4.30pm or Wednesday at 10am. Which one would you like to book?',
+    'Thursday isn’t available. You can choose between Tuesday at 4.30pm or Wednesday at 10am. Which one would you like to book?',
+    'OK. Your cardiology appointment has been rescheduled for Wednesday 10th July at 10am. Is there anything else?'
+  ]
+
   Twilio::TwiML::Response.new do |r|
-    r.Say 'What do you want to do?'
-    r.Pause length: 5
-    r.Say 'Are you Lucy?'
+    lines.each { |line|
+      r.Say line
+      r.Pause length: 4
+    }
+
   end.text
 end
